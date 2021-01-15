f2ee6dec7027921959ecabcdaf903cf27ca87290
Calum Mackervoy <c.mackervoy@gmail.com>
2021-01-15 22:47:22 +0000

    returning list of settlements from remote url

--- src/main/java/com/mackervoy/calum/mud/SettlementController.java
+++ src/main/java/com/mackervoy/calum/mud/SettlementController.java
@@ -1,13 +1,11 @@
 package com.mackervoy.calum.mud;
 
 import java.io.ByteArrayOutputStream;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.rdf.model.*;
 
 @Path("/settlements")
@@ -17,14 +15,9 @@ public class SettlementController {
     @Produces(MediaType.TEXT_PLAIN)
     public String sayHtmlHello() {
     	Model m = ModelFactory.createDefaultModel();
-
-        String NS = "https://example.com/test/";
-
-        Resource r = m.createResource(NS + "r");
-        Property p = m.createProperty(NS + "p");
-
-        r.addProperty(p, "hello world!", XSDDatatype.XSDstring);
         
+        m.read("https://calum.inrupt.net/public/collections/settlements.ttl");
+    	
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         
         m.write(baos, "Turtle");

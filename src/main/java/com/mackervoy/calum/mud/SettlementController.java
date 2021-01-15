9abcf6a130236d8b217ef853a801521ac72cddae
Calum Mackervoy <c.mackervoy@gmail.com>
2021-01-15 22:27:52 +0000

    Jena hello world

--- src/main/java/com/mackervoy/calum/mud/SettlementController.java
+++ src/main/java/com/mackervoy/calum/mud/SettlementController.java
@@ -1,16 +1,34 @@
 package com.mackervoy.calum.mud;
 
+import java.io.ByteArrayOutputStream;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.*;
+
 @Path("/settlements")
 public class SettlementController {
     @GET
     //@Produces("text/turtle")
-    @Produces(MediaType.TEXT_HTML)
+    @Produces(MediaType.TEXT_PLAIN)
     public String sayHtmlHello() {
-        return "Hello World!";
+    	Model m = ModelFactory.createDefaultModel();
+
+        String NS = "https://example.com/test/";
+
+        Resource r = m.createResource(NS + "r");
+        Property p = m.createProperty(NS + "p");
+
+        r.addProperty(p, "hello world!", XSDDatatype.XSDstring);
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        
+        m.write(baos, "Turtle");
+    	
+        return baos.toString();
     }
 }

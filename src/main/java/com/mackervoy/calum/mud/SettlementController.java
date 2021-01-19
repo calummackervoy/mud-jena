1755a9d2fcb2fc290ba8dfe93c9c0e35b511925c
Calum Mackervoy <c.mackervoy@gmail.com>
2021-01-19 21:32:46 +0000

    settlements endpoint serving local resource

--- src/main/java/com/mackervoy/calum/mud/SettlementController.java
+++ src/main/java/com/mackervoy/calum/mud/SettlementController.java
@@ -1,23 +1,28 @@
 package com.mackervoy.calum.mud;
 
 import java.io.ByteArrayOutputStream;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.rdf.model.*;
+import com.mackervoy.calum.mud.MUDServlet;
 
-@Path("/settlements")
+@Path("/settlements/")
 public class SettlementController {
     @GET
     //@Produces("text/turtle")
     @Produces(MediaType.TEXT_PLAIN)
-    public String sayHtmlHello() {
+    public String getSettlements() {
     	Model m = ModelFactory.createDefaultModel();
         
-        m.read("https://calum.inrupt.net/public/collections/settlements.ttl");
-    	
+    	m.read(MUDServlet.PATH);
+    		
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         
         m.write(baos, "Turtle");

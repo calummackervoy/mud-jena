0252bce7bad2c563a252aec4bcf8b23d6233d6c2
Calum Mackervoy <c.mackervoy@gmail.com>
2021-01-25 12:36:29 +0000

    Started adding vocabulary LIT for Time vocab

---
+++ src/main/java/com/mackervoy/calum/mud/vocabularies/Time.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.mackervoy.calum.mud.vocabularies;
+
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+
+/**
+ * @author Calum Mackervoy
+ * Class for accessing Resources and Properties of the Time ontology as constants
+ */
+public class Time {
+	public static final String uri ="http://www.w3.org/2006/time#";
+	
+	/** returns the URI for this schema
+     * @return the URI for this schema
+     */
+    public static String getURI() {
+          return uri;
+    }
+    
+    protected static final Resource resource( String local )
+    { return ResourceFactory.createResource( uri + local ); }
+
+    protected static final Property property( String local )
+    { return ResourceFactory.createProperty( uri, local ); }
+    
+    public final static Resource Instant = resource( "Instant" );
+    public final static Resource Interval = resource( "Interval" );
+    
+    public final static Property hasBeginning = property( "hasBeginning" );
+    public final static Property hasEnd = property( "hasEnd" );
+    public final static Property inXSDDate = property( "inXSDDate" );
+    public final static Property inXSDDateTimeStamp = property( "inXSDDateTimeStamp" );
+}

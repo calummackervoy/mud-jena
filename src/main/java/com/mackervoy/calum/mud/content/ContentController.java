97db1427488dbba219efaca38240e2ea007cce58
Matt Tennison <matt@matttennison.com>
2021-02-14 17:05:29 +0000

    Refactor ContentController

--- src/main/java/com/mackervoy/calum/mud/content/ContentController.java
+++ src/main/java/com/mackervoy/calum/mud/content/ContentController.java
@@ -7,6 +7,8 @@ import com.mackervoy.calum.mud.MUDApplication;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -28,29 +30,27 @@ public class ContentController {
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String getContent(@QueryParam("uri") String uri) {
-    // bounds checking - could provide a catch-all describer this way
-    if (uri == null) uri = "";
+		final String safeUri = uri == null ? "" : uri;
+		final Model m = ModelFactory.createDefaultModel();
+    m.read(safeUri);
 
-    // check if I have a describer for the URI
+		return getDescriber(safeUri, m)
+			.map(describer -> describer.describe(m, safeUri))
+			.orElse(null);
+  }
+	
+	private Optional<IContentDescriber> getDescriber(String uri, Model m) {
+		// check if I have a describer for the URI
     DescriberFactory factory = new DescriberFactory();
     IContentDescriber describer = factory.getDescriber(uri);
-    Model m = ModelFactory.createDefaultModel();
-    m.read(uri);
-
-    // if I don't, try the RDF type
-    if (describer == null) {
+		
+		// if I don't, try the RDF type
+		if (describer == null) {
       Resource r = m.getResource(uri);
       String type = r.getPropertyResourceValue(RDF.type).toString();
-      System.out.println("Retrieved type: " + type);
       describer = factory.getDescriber(type);
     }
 
-    // found a Describer, return 200 with content
-    if (describer != null) {
-      return describer.describe(m, uri);
-    }
-
-    // could not find a describer - return 204
-    return null;
-  }
+		return Optional.ofNullable(describer);
+	}
 }

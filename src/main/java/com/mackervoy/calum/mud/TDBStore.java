c81c4fe9a2d6ef1c1fb019ebce9e62f4cda256a2
Calum Mackervoy <c.mackervoy@gmail.com>
2021-02-21 16:58:18 +0000

    ResourceItem class

--- src/main/java/com/mackervoy/calum/mud/TDBStore.java
+++ src/main/java/com/mackervoy/calum/mud/TDBStore.java
@@ -10,7 +10,7 @@ import java.io.File;
  * A central class which manages access to directories and sub-directories of the dataset store
  */
 public class TDBStore {
-	public final static DatasetCollection WORLD = new DatasetCollection("/world/");
-	public final static DatasetCollection ACTIONS = new DatasetCollection("/actions/");
-	public final static DatasetCollection TASK_ACTIONS = new DatasetCollection("/actions/tasks/");
+	public final static DatasetCollection WORLD = new DatasetCollection("world/");
+	public final static DatasetCollection ACTIONS = new DatasetCollection("actions/");
+	public final static DatasetCollection TASK_ACTIONS = new DatasetCollection("actions/tasks/");
 }

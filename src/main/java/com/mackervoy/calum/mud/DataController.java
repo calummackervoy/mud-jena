4ececef23a234ad4898764068c13c6326a0bf553
Calum Mackervoy <c.mackervoy@gmail.com>
2021-02-22 20:41:05 +0000

    DataController accepts any number of path parameters

--- src/main/java/com/mackervoy/calum/mud/DataController.java
+++ src/main/java/com/mackervoy/calum/mud/DataController.java
@@ -4,11 +4,13 @@
 package com.mackervoy.calum.mud;
 
 import java.io.ByteArrayOutputStream;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.PathSegment;
 
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.ReadWrite;
@@ -19,15 +21,21 @@ import org.apache.jena.tdb2.TDB2Factory;
  * @author Calum Mackervoy
  * A generic endpoint for GETting datasets and resources
  */
-@Path("/")
+@Path("/{any: .*}")
 public class DataController {
 	@GET
     @Produces("text/turtle")
-	@Path("/{dataset}/")
-    public String get(@PathParam("dataset") String datasetSubPath) {
+    public String get(@PathParam("any") List<PathSegment> segments) {
 		//TODO: check if the dataset exists and return 404 if not (rather than creating an empty one on each 'not found' request
 		//	in TDB1 you would use inUseLocation(..) but this is not provided by the TDB2Factory (needs research)
 		//  best to use incorporate this into the responsibilities of TDBStore
+		
+		String datasetSubPath = "";
+		
+		for(int i = 0; i < segments.size(); i++) {
+			datasetSubPath += "/" + segments.get(i).toString();
+		}
+		
 		String datasetPath = MUDApplication.getRootDirectory() + datasetSubPath;
     	Dataset dataset = TDB2Factory.connectDataset(datasetPath);
     	

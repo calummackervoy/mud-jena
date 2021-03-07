9feb85269b996cf0c2d124e8595f0259e8647450
Calum Mackervoy <c.mackervoy@gmail.com>
2021-03-07 20:19:14 +0000

    minimal inferencing about mud:Locatable in AbstractTaskActor POST

--- src/main/java/com/mackervoy/calum/mud/behaviour/task/AbstractTaskActor.java
+++ src/main/java/com/mackervoy/calum/mud/behaviour/task/AbstractTaskActor.java
@@ -1,14 +1,22 @@
 package com.mackervoy.calum.mud.behaviour.task;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.List;
 
+import org.apache.jena.ontology.OntModel;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
+import org.apache.jena.reasoner.rulesys.Rule;
 import org.apache.jena.tdb2.TDB2Factory;
 import org.apache.jena.vocabulary.RDF;
 
@@ -57,7 +65,29 @@ public abstract class AbstractTaskActor implements ITaskActor {
 	 * @throws NullPointerException on case of no match
 	 */
 	protected Resource getFirstResourceMatchingType(Model request, Resource type) {
-		ResIterator matches = request.listResourcesWithProperty(RDF.type, type);
+		//taken from https://jena.apache.org/documentation/inference/#RDFSPlusRules
+		//TODO: read from file: https://github.com/Multi-User-Domain/mud-jena/issues/9
+		String basicRDFRules = "[rdfs2:  (?x ?p ?y), (?p rdfs:domain ?c) -> (?x rdf:type ?c)]\n" + 
+				"[rdfs3:  (?x ?p ?y), (?p rdfs:range ?c) -> (?y rdf:type ?c)]\n" + 
+				"[rdfs6:  (?a ?p ?b), (?p rdfs:subPropertyOf ?q) -> (?a ?q ?b)]\n" + 
+				"[rdfs9:  (?x rdfs:subClassOf ?y), (?a rdf:type ?x) -> (?a rdf:type ?y)]";
+		//List<Rule> rules = Rule.parseRules(basicRDFRules);
+		List<Rule> rules = Rule.rulesFromURL("https://calum.inrupt.net/public/rules/rdfsbasicrules.txt");
+		
+		/*OntModel buildings = ModelFactory.createOntologyModel(OntModelSpec.);
+		buildings.read("https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudbuildings.ttl");
+		buildings.read("https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mud.ttl");*/
+		//OntModel mud = ModelFactory.createOntologyModel("https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mud.ttl");
+		//buildings.add(mud);
+		//schema.read("https://raw.githubusercontent.com/Multi-User-Domain/vocab/main/mudbuildings.ttl");
+		
+		GenericRuleReasoner reasoner = new GenericRuleReasoner(rules);
+		reasoner.setOWLTranslation(true);               // not needed in RDFS case
+		reasoner.setTransitiveClosureCaching(true);
+		//InfModel inf = ModelFactory.createInfModel(reasoner, buildings, request);
+		InfModel inf = ModelFactory.createInfModel(reasoner, request);
+		
+		ResIterator matches = inf.listResourcesWithProperty(RDF.type, type);
 		try {
 			Resource match = matches.next();
 			return match;

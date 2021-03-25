c53465eea4ad33dffbdd436f5a6608e50835cf07
Calum Mackervoy <c.mackervoy@gmail.com>
2021-03-25 15:28:14 +0000

    DatasetItem holds and saves model associated to dataset

--- src/main/java/com/mackervoy/calum/mud/DatasetItem.java
+++ src/main/java/com/mackervoy/calum/mud/DatasetItem.java
@@ -1,5 +1,11 @@
 package com.mackervoy.calum.mud;
 
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.tdb2.TDB2Factory;
+
 /**
  * @author Calum Mackervoy
  * String accessors for the URI, File system location and for generating new resource Ids of a Dataset stored in the MUD TDB
@@ -7,11 +13,17 @@ package com.mackervoy.calum.mud;
 public class DatasetItem extends AbstractFileStorageWrapper {
 	private String name;
 	
+	/**
+	 * A constructor for a new Dataset in a collection, generates a random name for it
+	 */
 	public DatasetItem(String collectionPath) {
 		super(collectionPath);
 		this.name = Random.getRandomUUIDString();
 	}
 	
+	/**
+	 * A constructor for an existing Dataset in a collection, reads the model data from the dataset's URI
+	 */
 	public DatasetItem(String collectionPath, String name) {
 		super(collectionPath);
 		this.name = name;
@@ -25,5 +37,34 @@ public class DatasetItem extends AbstractFileStorageWrapper {
 	@Override
 	public String getFileLocation() {
 		return super.getFileLocation() + this.name + "/";
+	}
+	
+	//TODO: wrapper for get named model
+	
+	public Model getModel() {
+		Dataset dataset = TDB2Factory.connectDataset(this.getFileLocation());
+		Model out = ModelFactory.createDefaultModel();
+		try {
+			dataset.begin(ReadWrite.READ);
+			out.add(dataset.getDefaultModel());
+		}
+		finally {
+			dataset.end();
+		}
+		
+		return out;
+	}
+	
+	public void writeModel(Model model) {
+		Dataset dataset = TDB2Factory.connectDataset(this.getFileLocation());
+		try {
+			dataset.begin(ReadWrite.WRITE);
+			Model out = dataset.getDefaultModel();
+			out.add(model);
+			out.commit();
+		}
+		finally {
+			dataset.end();
+		}
 	}
 }

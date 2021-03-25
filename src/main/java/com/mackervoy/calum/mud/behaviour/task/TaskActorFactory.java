b79e23355603043eccbbe5f8f9d5add6811ebee4
Calum Mackervoy <c.mackervoy@gmail.com>
2021-03-25 13:13:15 +0000

    TaskActorFactory.getActorWithExistingTask uses MUDLogic.taskImplements to find target actor

--- src/main/java/com/mackervoy/calum/mud/behaviour/task/TaskActorFactory.java
+++ src/main/java/com/mackervoy/calum/mud/behaviour/task/TaskActorFactory.java
@@ -6,10 +6,14 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Optional;
 
+import javax.ws.rs.BadRequestException;
+
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.vocabulary.RDF;
+
+import com.mackervoy.calum.mud.vocabularies.MUDLogic;
 
 /**
  * @author Calum Mackervoy
@@ -23,22 +27,22 @@ public class TaskActorFactory {
 	 * Registers a provider to an RDF type with String
 	 * @returns true if the object could be registered, false if another class has already registered
 	 */
-	public static boolean register(String rdfType, Class<? extends ITaskActor> actor) {
-		if(taskActors.put(rdfType, actor) == null) {
+	public static boolean register(String targetTaskClass, Class<? extends ITaskActor> actor) {
+		if(taskActors.put(targetTaskClass, actor) == null) {
 			return true;
 		}
 		return false;
 	}
 	
-	private Constructor<? extends ITaskActor> resolveActorClassConstructor(String rdfType, Class<?>... parameterTypes) throws NoSuchMethodException, SecurityException {
-		Class<? extends ITaskActor> actor = taskActors.get(rdfType);
+	private Constructor<? extends ITaskActor> resolveActorClassConstructor(String targetTaskClass, Class<?>... parameterTypes) throws NoSuchMethodException, SecurityException {
+		Class<? extends ITaskActor> actor = taskActors.get(targetTaskClass);
 		if(actor == null) return null;
 		return actor.getConstructor(parameterTypes);
 	}
 	
-	public Optional<ITaskActor> getActorWithNewTask(String rdfType) {
+	public Optional<ITaskActor> getActorWithNewTask(String targetTaskClass) {
 		try {
-			return Optional.of(this.resolveActorClassConstructor(rdfType).newInstance());
+			return Optional.of(this.resolveActorClassConstructor(targetTaskClass).newInstance());
 		}
 		catch (Exception e) {
 			System.out.println("Error in TaskActorFactory.getActorWithNewTask!");
@@ -49,10 +53,12 @@ public class TaskActorFactory {
 	
 	public Optional<ITaskActor> getActorWithExistingTask(Resource task) {
 		String taskUri = task.getURI();
-	    String rdfType = task.getPropertyResourceValue(RDF.type).toString();
+	    Resource targetTaskClass = task.getPropertyResourceValue(MUDLogic.taskImplements);
+	    
+	    if(targetTaskClass == null) throw new BadRequestException("The existing Task was found but it must have the property mudLogic:taskImplements");
 	    
 	    try {
-			return Optional.of(this.resolveActorClassConstructor(rdfType, String.class).newInstance(taskUri));
+			return Optional.of(this.resolveActorClassConstructor(targetTaskClass.toString(), String.class).newInstance(taskUri));
 		}
 		catch (Exception e) {
 			System.out.println("Error in TaskActorFactory.getActorWithExistingTask!");

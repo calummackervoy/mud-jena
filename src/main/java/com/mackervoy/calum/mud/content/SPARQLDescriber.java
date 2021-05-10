f11fade041c34d88915114befcdec035c36a15c9
Calum Mackervoy <c.mackervoy@gmail.com>
2021-05-10 22:41:40 +0100

    SPARQLDescriber returning subjects which describe a MUDCharacter

--- src/main/java/com/mackervoy/calum/mud/content/SPARQLDescriber.java
+++ src/main/java/com/mackervoy/calum/mud/content/SPARQLDescriber.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.ws.rs.NotFoundException;
 
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.query.*;
 
@@ -32,21 +33,32 @@ public class SPARQLDescriber extends AbstractDescriber {
 		}
 	}
 	
-	protected void getContentDescribingObject(DatasetItem contentDataset, Resource r) {
+	protected Optional<Model> getContentDescribingObject(DatasetItem contentDataset, Resource r) {
 		// Select all triples where the subject describes a character
 		// TODO: prioritise those triples which target *this character*
 		String queryString = "SELECT ?s WHERE { ?s <" + MUDContent.describes.toString() + "> <" + MUDCharacter.Character.toString() + "> }";
 		Query query = QueryFactory.create(queryString);
+		
+		Model result = ModelFactory.createDefaultModel();
+		Model contentGraph = contentDataset.getModel();
 	  
-		try (QueryExecution qexec = QueryExecutionFactory.create(query, contentDataset.getModel())) {
+		try (QueryExecution qexec = QueryExecutionFactory.create(query, contentGraph)) {
 		    ResultSet results = qexec.execSelect();
-		    ResultSetFormatter.out(System.out, results, query);
+		    
+		    while(results.hasNext()) {
+		    	Resource subject = results.nextSolution().getResource("s");
+		    	
+		    	// read the full subject into the result graph
+		    	// TODO: this could probably be more efficient
+		    	result.add(ModelFactory.createDefaultModel().read(subject.getURI()));
+		    }
 		}
+		
+		return result.isEmpty() ? Optional.empty() : Optional.of(result);
 	}
 	
 	public Optional<Model> describe(Resource agent, Resource r) {
-		this.getContentDatasetItem().ifPresent(datasetItem -> this.getContentDescribingObject(datasetItem, r));
-		
-		return Optional.empty();
+		return this.getContentDatasetItem()
+				.flatMap(datasetItem -> this.getContentDescribingObject(datasetItem, r));
 	}
 }
